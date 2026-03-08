cf65d362fc8e7e3ac7660da37cf1b388203e1153
Dmitrii <dmitrii.nazarov7703@gmail.com>
2026-03-08 03:14:52 +0300

    add parametrization

--- data/products.ts
+++ data/products.ts
@@ -1,12 +1,11 @@
-export const products = {
-  backpack: {
+export const products = [
+  {
     dataTest: 'add-to-cart-sauce-labs-backpack',
     label: 'Sauce Labs Backpack',
   },
-  bikeLight: {
+  {
     dataTest: 'add-to-cart-sauce-labs-bike-light',
     label: 'Sauce Labs Bike Light',
   },
-};
-
+];
 // в идеале иметь тут данные из апишки
--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -1,25 +1,69 @@
+// import { test, expect } from '@playwright/test';
+// import { LoginPage } from '../pages/login.page';
+// import { InventoryPage } from '../pages/inventory.page';
+// import { CartPage } from '../pages/cart.page';
+// import { users } from '../data/users';
+// import { products } from '../data/products';
+
+// test('user can an item to cart', async ({ page }) => {
+//   const loginPage = new LoginPage(page);
+//   const inventoryPage = new InventoryPage(page);
+//   const cartPage = new CartPage(page);
+
+//   // Login
+//   await loginPage.goto();
+//   await loginPage.login(users.standard.username, users.standard.password);
+
+//   await inventoryPage.addItemToCart(products.bikeLight.dataTest);
+
+//   // todo: убрать хардкодинг если это возможно в рамках учебного сайта
+//   await expect(inventoryPage.getCartBadge()).toHaveText('1');
+
+//   await inventoryPage.openCart();
+
+//   await expect(cartPage.getItemByName(products.bikeLight.label)).toBeVisible();
+// });
+
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 import { InventoryPage } from '../pages/inventory.page';
 import { CartPage } from '../pages/cart.page';
+
 import { users } from '../data/users';
 import { products } from '../data/products';
 
-test('user can an item to cart', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  const cartPage = new CartPage(page);
+test.describe('Add products to cart', () => {
+  let inventoryPage: InventoryPage;
+  let cartPage: CartPage;
 
-  // Login
-  await loginPage.goto();
-  await loginPage.login(users.standard.username, users.standard.password);
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    cartPage = new CartPage(page);
 
-  await inventoryPage.addItemToCart(products.bikeLight.dataTest);
+    await loginPage.goto();
+    await loginPage.login(users.standard.username, users.standard.password);
+  });
 
-  // todo: убрать хардкодинг если это возможно в рамках учебного сайта
-  await expect(inventoryPage.getCartBadge()).toHaveText('1');
+  for (const product of products) {
+    test(`user can add ${product.label} to cart`, async () => {
+      await inventoryPage.addItemToCart(product.dataTest);
 
-  await inventoryPage.openCart();
+      await inventoryPage.openCart();
 
-  await expect(cartPage.getItemByName(products.bikeLight.label)).toBeVisible();
+      await expect(cartPage.getItemByName(product.label)).toBeVisible();
+    });
+  }
+
+  test('user can add multiple products to cart', async () => {
+    for (const product of products) {
+      await inventoryPage.addItemToCart(product.dataTest);
+    }
+
+    await inventoryPage.openCart();
+
+    for (const product of products) {
+      await expect(cartPage.getItemByName(product.label)).toBeVisible();
+    }
+  });
 });
